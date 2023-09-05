ca34547b90aeb7ec3f8a984c17986be593f75cbd
Chetan Vegad <73273733+Chetan007-cell@users.noreply.github.com>
2023-09-05 12:59:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CryptoSearch from './CryptoSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoSearch />
+    </div>
+  );
+}
+
+export default App;
